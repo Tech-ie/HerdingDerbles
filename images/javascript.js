dab62e06831fdeaa48857156602a2932230bd96b
Techie <thetechgirlofficial@gmail.com>
2022-03-07 22:21:29 +0500

    initial commit

---
+++ images/javascript.js
@@ -0,0 +1,35 @@
+function navBar(){
+    document.write(
+        '<div class="top-bar">'+
+            '<div class="top-container">'+
+                '<a href="./index.html"><img class="logo" src="images/herdingderbles.gif" alt="tutmo logo"></a>'+
+                '<div class="top-home-text">Herding Derbles</div>'+
+
+                '<div class="navbar">'+
+                    '<ul>'+
+                        '<li>'+
+                            '<a href="./index.html">Home</a>'+
+                        '</li>'+
+                        '<li>'+
+                            '<a href="./about.html">About</a>'+
+                        '</li>'+
+                        '<li>'+
+                            '<a href="./pricing.html">Pricing</a>'+
+                        '</li>'+
+                    '</ul>'+
+                '</div>'+
+
+            '</div>'+
+        '</div>'
+    );
+}
+
+function footer(){
+    document.write(
+        '<div class="footer-bar">'+
+        
+            '<p>&copy; 2019 Herding Derbles. All Rights Reserved.</p>'+
+
+        '</div>'
+    );
+}
